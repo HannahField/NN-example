c74a58f830f45e4c30539d2ba93d0af429bc095f
Hannah <thathannahfield@gmail.com>
2022-09-06 17:49:07 +0200

    added picture import and processing

---
+++ src/training.jl
@@ -0,0 +1,27 @@
+include("utils.jl")
+include("layer.jl")
+include("network.jl")
+using Images
+using FileIO
+
+
+basePath = "./Data/T0/"
+digit = rand(30:39)
+currentPath = string(basePath,digit,"/")
+digits = readdir(currentPath)
+
+chosenPicture = rand(digits)
+picturePath = string(currentPath,chosenPicture)
+
+image = Gray.(FileIO.load(picturePath))
+
+resizedImage = ImageTransformations.imresize(image,(64,64),method=ImageTransformations.Linear())
+
+FileIO.save("./test.png",resizedImage)
+pixels = convert.(Float64,resizedImage)
+
+network = newNetwork([64*64,100,100,10])
+
+pixels = reshape(pixels, (64*64))
+dump(chosenPicture)
+dump(evaluate(network,pixels))
